108a2d27a302234b49d0cc82e737f3a6230c8ebb
Horsey4 <horsey4@users.noreply.github.com>
2026-06-20 13:45:23 -0500

    Update to 26.2

--- src/main/java/horsey4/structureregiondebug/mixin/KeyboardHandlerMixin.java
+++ src/main/java/horsey4/structureregiondebug/mixin/KeyboardHandlerMixin.java
@@ -6,9 +6,11 @@ import horsey4.structureregiondebug.entry.StructureRegionExtentsRenderer;
 import net.minecraft.client.KeyboardHandler;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.input.KeyEvent;
+import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -20,7 +22,12 @@ public abstract class KeyboardHandlerMixin {
     private Minecraft minecraft;
 
     @Shadow
-    protected abstract void debugFeedbackTranslated(String pattern, Object... args);
+    protected abstract void debugFeedbackComponent(Component component);
+
+    @Unique
+    private void debugFeedbackTranslated(String pattern, Object... args) {
+        debugFeedbackComponent(Component.translatable(pattern, args));
+    }
 
     @Inject(method = "handleDebugKeys", at = @At("TAIL"), cancellable = true)
     private void checkStructureRegionBorderRendererKeybinds(KeyEvent event, CallbackInfoReturnable<Boolean> cir) {
